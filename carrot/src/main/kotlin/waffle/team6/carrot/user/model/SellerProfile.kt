fa2d2e1068b6de33859b6f76196074746cbe19fc
pullup-dip-burpee <qazxdrtgbnj@gmail.com>
2021-12-04 16:02:28 +0900

    user-1 Feat: Add fields to model and dto

--- carrot/src/main/kotlin/waffle/team6/carrot/user/model/SellerProfile.kt
+++ carrot/src/main/kotlin/waffle/team6/carrot/user/model/SellerProfile.kt
@@ -3,10 +3,13 @@ package waffle.team6.carrot.user.model
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.OneToOne
 
 @Entity
 class SellerProfile(
     @Id @GeneratedValue
     val id: Long = 0,
-) {
-}
+
+    @OneToOne
+    val user: User,
+)
