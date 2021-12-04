af61055a2bc3bc4bbeae1885e3f17ba42da5cd92
pullup-dip-burpee <qazxdrtgbnj@gmail.com>
2021-12-04 20:12:29 +0900

    user-1 Feat: Add email field to User

--- carrot/src/test/kotlin/waffle/team6/carrot/user/service/UserServiceTest.kt
+++ carrot/src/test/kotlin/waffle/team6/carrot/user/service/UserServiceTest.kt
@@ -9,14 +9,12 @@ import org.mockito.Mock
 import org.mockito.Mockito
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
-import org.springframework.transaction.annotation.Transactional
 import waffle.team6.carrot.user.dto.UserDto
 import waffle.team6.carrot.user.model.User
 import waffle.team6.carrot.user.repository.UserRepository
 
 @SpringBootTest
 @RunWith(SpringRunner::class)
-@Transactional
 internal class UserServiceTest {
 
     @Mock
@@ -31,17 +29,30 @@ internal class UserServiceTest {
         val signUpRequest = UserDto.SignUpRequest(
             name = "alice",
             password = "alicePassword",
+            email = "alice@carrot.com",
         )
         val newUser = User(
             name = "alice",
             password = "alicePassword",
+            email = "alice@carrot.com",
         )
-        Mockito.`when`(userRepository.save(Mockito.any(User::class.java))).thenReturn(newUser)
+        Mockito.`when`(userRepository.save(Mockito.any(User::class.java)))
+            .thenReturn(newUser)
 
         // when
         val createUser = userService.createUser(signUpRequest)
 
         // then
         Assertions.assertThat(createUser.name).isEqualTo(signUpRequest.name)
+    }
+
+    @Test
+    fun shouldSucceedUpdateUser() {
+        // given
+
+        // when
+//        Mockito.`when`()
+
+        //then
     }
 }
