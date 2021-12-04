2c618d7e7cf498475d875f4051be001d658942b9
pullup-dip-burpee <qazxdrtgbnj@gmail.com>
2021-12-05 02:12:44 +0900

    user-1 Feat: Use PasswordEncoder in UserService for sign up and sign in API

--- carrot/src/main/kotlin/waffle/team6/global/auth/model/UserPrincipal.kt
+++ carrot/src/main/kotlin/waffle/team6/global/auth/model/UserPrincipal.kt
@@ -1,36 +1,37 @@
 package waffle.team6.global.auth.model
 
 import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 import waffle.team6.carrot.user.model.User
 import java.util.*
 
 class UserPrincipal(val user: User): UserDetails {
     override fun getAuthorities(): List<GrantedAuthority> {
-        TODO("Not yet implemented")
-    }
-
-    override fun getPassword(): String {
-        TODO("Not yet implemented")
+        return listOf(SimpleGrantedAuthority("normal"))
     }
 
     override fun getUsername(): String {
-        TODO("Not yet implemented")
+        return user.name
+    }
+
+    override fun getPassword(): String {
+        return user.password
     }
 
     override fun isAccountNonExpired(): Boolean {
-        TODO("Not yet implemented")
+        return true
     }
 
     override fun isAccountNonLocked(): Boolean {
-        TODO("Not yet implemented")
+        return true
     }
 
     override fun isCredentialsNonExpired(): Boolean {
-        TODO("Not yet implemented")
+        return true
     }
 
     override fun isEnabled(): Boolean {
-        TODO("Not yet implemented")
+        return true
     }
 }
