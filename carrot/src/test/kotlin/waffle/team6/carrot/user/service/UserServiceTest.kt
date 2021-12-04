efb56f76edfbdc834adf5fb22ea93fbbd282c136
pullup-dip-burpee <qazxdrtgbnj@gmail.com>
2021-12-05 02:16:19 +0900

    user-1 Test: Fix test

--- carrot/src/test/kotlin/waffle/team6/carrot/user/service/UserServiceTest.kt
+++ carrot/src/test/kotlin/waffle/team6/carrot/user/service/UserServiceTest.kt
@@ -1,12 +1,15 @@
 package waffle.team6.carrot.user.service
 
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.runner.RunWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.test.annotation.Rollback
 import org.springframework.test.context.junit4.SpringRunner
 import waffle.team6.carrot.user.dto.UserDto
 import waffle.team6.carrot.user.model.User
@@ -14,7 +17,11 @@ import waffle.team6.carrot.user.repository.UserRepository
 
 @SpringBootTest
 @RunWith(SpringRunner::class)
+@Rollback
 internal class UserServiceTest {
+
+    @Mock
+    private lateinit var passwordEncoder: PasswordEncoder
 
     @Mock
     private lateinit var userRepository: UserRepository
@@ -22,23 +29,20 @@ internal class UserServiceTest {
     @InjectMocks
     private lateinit var userService: UserService
 
+    @BeforeEach
+    fun beforeEach() {
+
+    }
+
     @Test
     fun shouldSucceedSignUp() {
         // given
-        val signUpRequest = UserDto.SignUpRequest(
-            name = "alice",
-            password = "alicePassword",
-            email = "alice@carrot.com",
-            phone = "010-0000-0000"
-        )
-        val newUser = User(
-            name = "alice",
-            password = "alicePassword",
-            email = "alice@carrot.com",
-            phone = "010-0000-0000"
-        )
+        val signUpRequest = getSignUpRequestOfAlice()
+        val newUser = getUserEntityOfAlice()
         Mockito.`when`(userRepository.save(Mockito.any(User::class.java)))
             .thenReturn(newUser)
+        Mockito.`when`(passwordEncoder.encode(Mockito.anyString()))
+            .thenReturn("EncodedPassword")
 
         // when
         val createUser = userService.createUser(signUpRequest)
@@ -55,5 +59,23 @@ internal class UserServiceTest {
 //        Mockito.`when`()
 
         //then
+    }
+
+    private fun getSignUpRequestOfAlice(): UserDto.SignUpRequest {
+        return UserDto.SignUpRequest(
+            name = "alice",
+            password = "alicePassword",
+            email = "alice@carrot.com",
+            phone = "010-0000-0000"
+        )
+    }
+
+    private fun getUserEntityOfAlice(): User {
+        return User(
+            name = "alice",
+            password = "EncodedPassword",
+            email = "alice@carrot.com",
+            phone = "010-0000-0000"
+        )
     }
 }
