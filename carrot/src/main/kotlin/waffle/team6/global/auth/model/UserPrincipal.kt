0ab52308757af8befeb8b35f54344714e32cf33a
pullup-dip-burpee <qazxdrtgbnj@gmail.com>
2021-12-04 20:37:17 +0900

    user-1 Feat: Init UserPrincipal

---
+++ carrot/src/main/kotlin/waffle/team6/global/auth/model/UserPrincipal.kt
@@ -0,0 +1,35 @@
+package waffle.team6.global.auth.model
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import waffle.team6.carrot.user.model.User
+
+class UserPrincipal(val user: User): UserDetails {
+    override fun getAuthorities(): List<GrantedAuthority> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPassword(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUsername(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun isAccountNonExpired(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isAccountNonLocked(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isCredentialsNonExpired(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isEnabled(): Boolean {
+        TODO("Not yet implemented")
+    }
+}
