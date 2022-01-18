37be66d519c71c4f627a4c030d4921833a7c4bdf
pullup-dip-burpee <qazxdrtgbnj@gmail.com>
2022-01-19 01:52:20 +0900

    kakao-login Feat: Init KakaoClient(feign client)

---
+++ carrot/src/main/kotlin/waffle/team6/carrot/user/kakao/KakaoConf.kt
@@ -0,0 +1,7 @@
+package waffle.team6.carrot.user.kakao
+
+object KakaoConf {
+    const val CLIENT_ID: String = "asdf"
+    const val REDIRECT_URI: String = "asdf"
+    const val GRANT_TYPE: String = "authorization_code"
+}
